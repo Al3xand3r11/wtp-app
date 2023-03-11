44ae00e78ea32aa636c9faaddeb4a42ed04fe8e6
Al3xand3r11 <alexander.nance11@gmail.com>
2023-03-10 23:54:18 -0800

    getting background to show

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
-import Home from './Components/Home';
+
+import Home from "./Components";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
