f1ef501aeff60d9b2f15230a1aec56ed569ffc3f
Al3xand3r11 <alexander.nance11@gmail.com>
2023-03-10 23:54:46 -0800

    moving files

--- src/Components/Members.jsx
+++
@@ -1,7 +0,0 @@
-const Members = () => {
-    return (
-        <></>
-    );
-};
-
-export default Members;
--- src/Styles/Home-styled.jsx
+++
@@ -1,5 +0,0 @@
-import styled from 'styled-components';
-
-export const homeContainer = styled.home`
-background-color: #000000;
-`
